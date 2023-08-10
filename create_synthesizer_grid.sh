2c028ae0a21fc46c1a69cbbaa04cc1c37e13137c
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-08-10 08:28:12 +0100

    updated script

--- create_synthesizer_grid.sh
+++ create_synthesizer_grid.sh
@@ -5,4 +5,4 @@ synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
 grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy-c17.03"
 
 cd ..
-python3 create_synthesizer_grid.py -grid $grid -dir $synthesizer_dir
+python3 create_synthesizer_grid.py -grid_name $grid -synthesizer_data_dir $synthesizer_dir -line_calc_method linelist
